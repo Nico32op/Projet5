6e961b07047f05328e725af7831a8f9cd7dc9ae0
Nico32op <leonardi.nicolas@live.fr>
2021-03-11 14:15:45 +0100

    maj 14h15 11-03-21

--- front/panier.js
+++ front/panier.js
@@ -65,7 +65,7 @@ alert("Produit Supprimé =(");
 
 let paniermontantotal = [] ; //on déclare un tableau qui contiendra chaque montant du panier
 
-for(let k = 0; k < produitdanslocalstorage.length; k++){
+for(let k = 0 ; k < produitdanslocalstorage.length; k++){
 let prixproddanspanier = produitdanslocalstorage[k].prix;
 console.log  (prixproddanspanier) //affiche le prix des produits dans le local storage/panier    
 
@@ -91,19 +91,19 @@ const affichformulairehtml = `
 <h1 class="titreformulaire">Formulaire de commande</h1>
 <form>
   <label for="nom">Nom</label>
-  <input type="text" id="nom" name="nom" maxlength='5' placeholder="Votre nom" required>
+  <input type="text" id="nom" name="nom" placeholder="Votre nom" required>
 
   <label for="prenom">Prénom</label>
   <input type="text" id="prenom" name="prenom" placeholder="Votre prénom">
 
-  <label for="mail">Email</label>
-  <input type="email" id="email" name="email" placeholder="Votre email" required>
+  <label for="email">Email</label>
+  <input id="email" type="email" name="email" placeholder="Email" required>
   
   <label for="ville">Ville</label>
   <input type="text" id="ville" name="ville"  placeholder="Votre ville" >
 
   <label for="cp">Code Postal</label>
-  <input type="text"  id="cp" name="cp" placeholder="Votre code Postal"  pattern="[0-9]{,5}">
+  <input type="text"  id="cp" name="cp" placeholder="Votre code Postal">
    
   <label for="adresse">Adresse</label>
   <textarea id="adresse" name="adresse" placeholder="Votre adresse" style="height:50px"></textarea>
@@ -119,16 +119,14 @@ constplacementformulairehtml.innerHTML =affichformulairehtml ;
 const btnenvoieformulaire = document.querySelector("#envoieformulaire"); //selection du bouton envoie formulaire
 btnenvoieformulaire.addEventListener("click", function(e){ //création de ce qui est effectué au click
 e.preventDefault(); //empeche le recharge de la console au clic
-
-const formulaireok = {//création d'une constante qui contient/affichera toutes les valeurs du formulaires ensenmble 
+checkInput ();
+/* const formulaireok = {//création d'une constante qui contient/affichera toutes les valeurs du formulaires ensenmble 
 nom : document.querySelector("#nom").value, 
 prenom : document.querySelector("#prenom").value, 
 email : document.querySelector("#email").value, 
 ville : document.querySelector("#ville").value, 
 cp : document.querySelector("#cp").value, 
 adresse : document.querySelector("#adresse").value
-
-
 };
 console.log(formulaireok);
 
@@ -141,14 +139,10 @@ const Envoieserveur = {
     
 };
 console.log(Envoieserveur)
-
-console.log("fait") 
-
 //window.location.href = "panier.html"; //recharge l'url à l'envoie du formulaire 
 //alert("Commande Envoyé");
 
-localStorage.removeItem("teddy"); //permet de supprimer les articles du panier (clé teddy) a l'envoie du formulaire, cependant les informations restent dans la clé teddy 2
-
+localStorage.removeItem("teddy");  *///permet de supprimer les articles du panier (clé teddy) a l'envoie du formulaire, cependant les informations restent dans la clé teddy 2
 
  /*        let postfect = fetch("http://localhost:3000/api/teddies/order", {
             method: 'POST',
@@ -169,9 +163,96 @@ localStorage.removeItem("teddy"); //permet de supprimer les articles du panier (
 })
 
 
- /* btnenvoieformulaire.addEventListener("click", function(e){ //création de ce qui est effectué au click
-e.preventDefault(); //empeche le recharge de la console au clic
-if (window.getComputedStyle(document.querySelector("#cardlistpanier")).display=='block'){
-    document.querySelector("#cardlistpanier").style.display="none";
-    }  
-})     */
+
+checkInput = () =>{
+    //Controle Regex
+    let checkString = /[a-zA-Z]/;
+    let checkNumber = /[0-9]/;
+    //Source pour vérification email => emailregex.com
+    let checkMail = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/y;
+    let checkSpecialCharacter = /[§!@#$%^&*(),.?":{}|<>]/;
+
+    //message fin de controle
+    let checkMessage = "";
+
+    //Récupération des inputs
+    let Nom = document.querySelector("#nom").value;
+    let Prenom = document.querySelector("#prenom").value;
+    let Email = document.querySelector("#email").value;
+    let Adresse = document.querySelector("#adresse").value;
+    let Ville = document.querySelector("#ville").value;
+
+
+      //tests des différents input du formulaire
+        //Test du nom => aucun chiffre ou charactère spécial permis
+        if(checkNumber.test(Nom) == true || checkSpecialCharacter.test(Nom) == true || Nom == ""){
+        	checkMessage = "Vérifier/renseigner votre nom";
+        }else{
+        	console.log("Administration : Nom ok");
+        };
+        //Test du nom => aucun chiffre ou charactère spécial permis
+        if(checkNumber.test(Prenom) == true || checkSpecialCharacter.test(Prenom) == true || Prenom == ""){
+        	checkMessage = checkMessage + "\n" + "Vérifier/renseigner votre prénom";
+            console.log("aa")
+        }else{
+        	console.log("Prénom ok");
+        };
+        //Test du mail selon le regex de la source L256
+        if(checkMail.test(Email) == false){
+        	checkMessage = checkMessage + "\n" + "Vérifier/renseigner votre email";
+        }else{
+        	console.log("Adresse mail ok");
+        };
+        //Test de l'adresse => l'adresse ne contient pas obligatoirement un numéro de rue mais n'a pas de characteres spéciaux
+        if(checkSpecialCharacter.test(Adresse) == true || Adresse == ""){
+        	checkMessage = "Vérifier/renseigner votre adresse";
+        }else{
+        	console.log("Administration : Adresse ok");
+        };
+        //Test de la ville => aucune ville en France ne comporte de chiffre ou charactères spéciaux
+        if(checkSpecialCharacter.test(Ville) == true && checkNumber.test(Ville) == true || Ville == ""){
+        	checkMessage = checkMessage + "\n" + "Vérifier/renseigner votre ville"
+        }else{
+        	console.log("Administration : Ville ok")
+        };
+        //Si un des champs n'est pas bon => message d'alert avec la raison
+        if(checkMessage != ""){
+        	alert("Il est nécessaire de :" + "\n" + checkMessage);
+        }
+        //Si tout est ok construction de l'objet contact => a revoir
+        else{const formulaireok = {//création d'une constante qui contient/affichera toutes les valeurs du formulaires ensenmble 
+        nom : document.querySelector("#nom").value, 
+        prenom : document.querySelector("#prenom").value, 
+        email : document.querySelector("#email").value, 
+        ville : document.querySelector("#ville").value, 
+        cp : document.querySelector("#cp").value, 
+        adresse : document.querySelector("#adresse").value
+        };
+        console.log(formulaireok);
+        
+        localStorage.setItem("formulaireok", JSON.stringify(formulaireok)); //création d'une clé au format json dans le local storage qui contient les info du formulaire
+        
+        //donné a envoyer serveur, formulaire + produit enregistré dans le panier(local storage)
+        const Envoieserveur = { 
+            produitdanslocalstorage, 
+            formulaireok
+            
+        };
+        console.log(Envoieserveur)
+        //window.location.href = "panier.html"; //recharge l'url à l'envoie du formulaire 
+        //alert("Commande Envoyé");
+        
+        localStorage.removeItem("teddy"); //permet de supprimer les articles du panier (clé teddy) a l'envoie du formulaire, cependant les informations restent dans la clé teddy 2
+                //window.location.href = "panier.html"; //recharge l'url à l'envoie du formulaire 
+                //alert("Commande Envoyé");
+        	    contact = {
+        		Nom : Nom,
+        		Prenom : Prenom,
+        		Adresse : Adresse,
+        		Ville : Ville,
+        		Email : Email
+        	};
+        	return contact;
+        };
+    }; 
+
