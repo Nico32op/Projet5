813bc675f0bfc1275245bce19a7df3e78fdec33a
Nico32op <leonardi.nicolas@live.fr>
2021-03-17 16:41:30 +0100

    maj 16h41

--- front/confirm.js
+++ front/confirm.js
@@ -1,28 +1,32 @@
-let produitdanslocalstorage = JSON.parse(localStorage.getItem("teddy2")); //récup du local storage dans la page panier avec la clé teddy
-console.log(produitdanslocalstorage);
+let contactstorage = JSON.parse(localStorage.getItem("contact")); //récup du local storage les infos clients
+console.log(contactstorage.lastName);  //.parse permet de traduit le format json en format js
 
 let montanttotalstorage =  JSON.parse(localStorage.getItem("montanttotal")); //récup du montant total du storage 
-console.log(montanttotalstorage);
+//console.log(montanttotalstorage);
 
 let retourserveur = JSON.parse(localStorage.getItem("retourserveurstorage")); //récup du retour serveur storage 
-console.log(retourserveur);
+//console.log(retourserveur);
 
-for (let q = 0; q < montanttotalstorage.length; q++) { //je vais cherche le contenu montant de mon local storage
-    let infomontant = montanttotalstorage[q]; //je sauvegarde le montant dans une variable
-    console.log(infomontant) 
-}
+
+    for (let j = 0; j < retourserveur.length; j++) { //je vais chercher le contenu du retour serveur de mon local storage
+    let infoserveur = retourserveur[j]; //je sauvegarde le contenu dans une variable
+    let idserveur = retourserveur[j].orderId; // je sauvegarder l'id du retour serveur dans une variable
+    console.log(idserveur)
+    console.log(infoserveur) 
+
 
 const cardlistconfirm = document.querySelector("#cardlistconfirm"); //je seletionne l'endroit ou je veux afficher les infos sur la page html
 const cardconfirm = 
 `<article class="prodselectionner"> 
   <div class="card-body">
-  <img class="card-img-top" src="" alt="teddy"/>
-    <h5 class="card-title"><button type="button" id="plus">+</button>Quantité : <span id="afficheQuantite"></span><button type="button" id="moin">-</button <br><br>Nom : </h5>
-    <h5 class="card-title">Prix à l'unité : </h5>
-    <h5 class="card-prix">Prix Total : <span id="price">${montanttotalstorage}</span>euros<p><a id="btn_supp" href="#" role="button">Supprimer</a></p></h5> 
+  <img class="card-img-top" src="images/homer.jpg" alt="snif"/>
+    <h5 class="card-title">${contactstorage.lastName} ${contactstorage.firstName} merci pour votre confiance!</h5>
+    <h5 class="card-title">Le numéro de suivi de votre commande est : ${idserveur}</h5> 
+    <h5 class="card-prix">Le montant de total de votre achat est de : <span id="price">${montanttotalstorage}</span>euros<p><a id="btn_supp" href="#" role="button">Supprimer</a></p></h5> 
   </div>
 </article>
 `
 ;
 
 cardlistconfirm.innerHTML=cardconfirm; //j'insère dans la partie html les infos choisies à l'endroit ciblé 
+    }
